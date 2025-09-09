d1b28f2293027eaca393d1938badfbcaed0b4397
Bpkarki19 <bipinkarki9861@gmail.com>
2025-09-09 17:29:12 +0545

    update 4

--- swiper.js
+++ swiper.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   const myButton = document.getElementById('my_button');
+  const moreBrands = document.querySelector('.more--brands');
+  const computedStyle = window.getComputedStyle(moreBrands);
   if (myButton) {
     myButton.addEventListener('click', function() {
       alert('Button clicked!');
+      const display =
     });
   } else {
     console.error('Button with id "my_button" not found.');
