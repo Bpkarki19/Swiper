42bd4d89694e69adac2e6f1a17e63fd4193aa21a
Bpkarki19 <bipinkarki9861@gmail.com>
2025-09-11 20:05:50 +0545

    refactor: update swiper styles and structure, remove unused HTML file

--- swiper.js
+++ swiper.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
   const toggleButton = document.getElementById('toggle_button');
   const moreBrands = document.querySelectorAll('.grid-item.hidden');
+  console.log(moreBrands);
+  //moreBrands is a NodeList of all elements with the class 'grid-item' and 'hidden'. it behaves like an array of DOM elements.
 
   if (toggleButton) {
     toggleButton.addEventListener('click', () => {
       const isHidden = moreBrands[0] && moreBrands[0].classList.contains('hidden');
+      console.log(moreBrands[0]);
+      //1. trying to access the first element in that array.
+      //2. If the first operand (moreBrands[0]) is truthy, then the second operand (moreBrands[0].classList.contains('hidden')) is evaluated. 
+      //3. This part will only be evaluated if moreBrands[0] is truthy (due to the && operator).
 
-      moreBrands.forEach(brand => {
+      moreBrands.forEach(brand => {// iterating over each element in the moreBrands NodeList.
         if (isHidden) {
           brand.classList.remove('hidden');
         } else {
